fa5ec9d00cac92cef022e67e5b8d219d9a1555d8
Nayani_Karthik <nayanikarthik961@gmail.com>
2025-01-30 09:03:56 +0530

    add all changes

--- index.js
+++ index.js
@@ -29,3 +29,42 @@ function DEL() {
   result = result.slice(0, -1);
   display.value = result;
 }
+
+// let display = document.getElementById("display");
+// let result = "";
+// let operators = "+-*/%.";
+
+// function sym(event) {
+//   let lastChar = result.slice(-1);
+//   let newChar = event.target.value;
+
+//   if (!operators.includes(lastChar) || !operators.includes(newChar)) {
+//     result += newChar;
+//     display.value = result;
+//   }
+// }
+
+// function calc(event) {
+//   result += event.target.value;
+//   display.value = result;
+// }
+
+// function str() {
+//   try {
+//     result = eval(result);
+//     display.value = result;
+//   } catch (error) {
+//     display.value = "Error";
+//     result = "";
+//   }
+// }
+
+// function AC() {
+//   result = "";
+//   display.value = result;
+// }
+
+// function DEL() {
+//   result = result.slice(0, -1);
+//   display.value = result;
+// }
